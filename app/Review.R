6e43287328b3aebefc94832412cb3b9412fff174
Tom Weishaar <tom.weishaar@gmail.com>
2018-07-30 20:18:49 -0400

    Major update
    Review.R now has a working Dashboard and Citation List

--- app/Review.R
+++ app/Review.R
@@ -16,17 +16,23 @@ source("chokidar.R", local=TRUE)
 S$REV <- list()
 S$WHERE <- tibble(c("catalogID", ">", 0))  # default WHERE
 
-S$PGN$start <- '<nav aria-label="Citation list pagination"><ul class="pagination"><li class="page-item"><a id="p_0" class="page-link">First Page</a></li>'
-S$PGN$end <- '<li class="page-item"><a id="p_999" class="page-link">Last Page</a></li></ul></nav>'
+S$PGN$FR = recGet(S$db, "catalog", c("catalogID", "reviewBest"), tibble(c("dupOf", "=", 0)))
+S$PGN$norecs = S$PGN$FR$catalogID[1] == 0
+S$PGN$filteredIDs = S$PGN$FR$catalogID
+S$PGN$chunkedIDs = chunker(S$PGN$filteredIDs, 30)
+S$PGN$pageCount = length(S$PGN$chunkedIDs)
+S$PGN$activePage = 1
+
+S$fil$abstract = ""
+S$fil$author = ""
+S$fil$year = ""
+S$fil$journal = ""
+S$fil$review_0 = TRUE
+S$fil$review_1 = TRUE
+S$fil$review_2 = TRUE
 
 # Init reactive variables
 rv$render = 0         # Trigger to render page after dealing with input initializatons
-
-# Only needed on this page:
-# catalogGet = function(db=S$db, SELECT="", WHERE=S$WHERE) {
-#    return(recGet(db, "catalog", SELECT, WHERE))
-# }
-
 rv$menuActive = 1    # Start out on first sub-menu
 #####
 
@@ -42,68 +48,64 @@ rv$menuActive = 1    # Start out on first sub-menu
 # })
 
 
-S$PGN$F <- recGet(S$db, "catalog", c("catalogID", "reviewBest"), tibble(c("catalogID", "<", 10)))
-
-
-pages = c(5:9)
-activePage = 7
-S$PGN$middle <- ""
-for(p in pages) {
-   S$PGN$middle <- paste0(S$PGN$middle, ifelse(p==activePage, '<li class="page-item active"><a id="', '<li class="page-item"><a id=p_"'),
-                          p, '" class="page-link">', p, '</a></li>')
-}
 
 #if(rv$limn && S$P$Msg=="") {
 if(S$P$Msg=="") {
-   r = recGet(S$db, "catalog", c("catalogID", "title", "author", "journal", "Y", "reviewBest", "reviewCount"), tibble(c("catalogID", "<", 10)))
-   if(S$P$Modify) {
-      r$btn = paste0("<button id=cat_", r$catalogID, " class='btn border-dark btn-success'>Review</button>")
-   } else {
-      r$btn = paste0("<button id=cat_", r$catalogID, " class='btn border-dark btn-primary'>View</button>")
-   }
    output$uiMeat <- renderUI({rv$render; isolate({
       switch(as.character(rv$menuActive),
          "1" = {
-            r = recGet(S$db, "catalog", c("catalogID", "reviewBest", "reviewCount"), tibble(c("catalogID", ">", 0)))
-            if(r$catalogID>0) {                                                            # Prepare data for doughnut charts
+            r = recGet(S$db, "catalog", c("catalogID", "dupOf", "reviewBest", "reviewCount"), tibble(c("catalogID", ">", 0)))
+            if(r$catalogID[1]>0) {                                                         # Prepare data for doughnut charts
                noText <- ""
-               aData <- c(sum(r$reviewBest==0), sum(r$reviewBest==1),sum(r$reviewBest==2))   # Have cites, chart 1
+               aData <- c(sum(r$reviewBest==0), sum(r$reviewBest==1),sum(r$reviewBest==2)) # Have cites, chart 1
                aColors <- c("#1997c6","#9F86FF","#1BC98E")
-               if(sum(r$reviewCount>0)==0) {                                           # Have cites, but no reviews, chart 2
+               cData <- c(sum(r$dupOf==0), sum(r$dupOf!=0))
+               cColors <- c("#1997c6","#9F86FF")
+               if(sum(r$reviewCount>0)==0) {                                               # Have cites, but no reviews, chart 2
                   bData <- c(1,1,1)
                   bColors <- c("#6c757d","#6c757d","#6c757d")
-               } else {                                                                # Have cites & reviews, chart 2
+               } else {                                                                    # Have cites & reviews, chart 2
                   bData <- c(sum(r$reviewCount==1), sum(r$reviewCount==2), sum(r$reviewCount>2))
                   bColors <- c("#1997c6","#9F86FF","#1BC98E")
                }
-            } else {                                                                   # No cites, no reviews, both charts
+            } else {                                                                       # No cites, no reviews, both charts
                noText <- "<h5>Nothing to review yet</h5>"
                aData <- c(1,1,1)
                aColors <- c("#6c757d","#6c757d","#6c757d")
                bData <- c(1,1,1)
                bColors <- c("#6c757d","#6c757d","#6c757d")
+               cData <- c(1,1)
+               cColors <- c("#6c757d","#6c757d")
             }
             restOfPage = tagList(
                uiOutput("dashboard")
             )
-            output$dashboard <- renderUI({rv$render; isolate({                         # Here come the doughnut charts
+            output$dashboard <- renderUI({rv$render; isolate({                             # Here come the doughnut charts
                tagList(
                   bs4("r",
-                     bs4("chart", c=6, id=paste0("cA"), labels=c("Not Reviewed", "Failed", "Passed"),
+                     bs4("chart", c=4, id=paste0("cC"), labels=c("Unique", "Duplicates"),
+                         data=cData,
+                         legend="false", zeroText=noText,
+                         title1="All Citations", title2="Unique vs</br>Duplicates",
+                         colors=cColors
+                     ),
+                     bs4("chart", c=4, id=paste0("cA"), labels=c("Not Reviewed", "Failed", "Passed"),
                          data=aData,
                          legend="false", zeroText=noText,
                          title1="Stage 1 Reviews", title2="Pass vs Fail vs<br>Not Reviewed",
                          colors=aColors
                      ),
-                     bs4("chart", c=6, id=paste0("cB"), labels=c("1 review", "2 reviews", "3 or more"),
+                     bs4("chart", c=4, id=paste0("cB"), labels=c("1 review", "2 reviews", "3 or more"),
                          data=bData,
                          legend="false", zeroText=noText,
-                         title1="Fails Only", title2="Number of<br>Reviews",
+                         title1="Stage 1 Fails Only", title2="Number of<br>Reviews",
                          colors=bColors
                      )
                   ),
-bs4("r", bs4("c1"), bs4("c10", bs4("cd", q="y", bs4("cdb", bs4("cdt", HTML0(
-"<p>The first graph shows reviewing progress and results. The second graph - which is for Stage 1 <b>Fails</b> only
+bs4("r", bs4("c1"), bs4("c10", bs4("cd", q="y", bs4("cdb", bs4("cdt", HTML0(               # The yellow box
+"<p>The first graph shows the number of duplicates that have been identified so far in the project's list of citations.
+The second graph shows reviewing progress and results.</p>
+<p>The third graph - which is for Stage 1 <b>Fails</b> only
 (the article does not meet project criteria) - divides the Fails into groups by how many times they've been reviewed.
 This graph shows <i>Fails</i> only because for <i>Not Reviewed</i> the number is zero and for <i>Stage 1 Pass</i>
 the number of reviews doesn't matter - any article passed by one reviewer moves on to Stage 2 Review.</p>
@@ -114,48 +116,67 @@ no data is available for that graph at this time (no searches have been processe
 })})
          },
          "2" = {
-            restOfPage = tagList(
-               uiOutput("showCatalog")
-            )
-            output$showCatalog <- renderUI({rv$render; isolate({
-               tagList(
-                  bs4("r",
-                     bs4("c9",
-                        HTML("<span style='font-size: 1.25rem; color:#fff;'>Filter citations</span><br>"),
-                        ttextInput("tabFilter", "Phrase in title or abstract", value="", groupClass="w-75"),
-                        bs4("r",
-                           bs4("c5",
-                              ttextInput("aFilter", "Author", value="", groupClass="w-100")),
-                           bs4("c2",
-                              ttextInput("YFilter", "Year", value="", groupClass="w-100")),
-                           bs4("c5",
-                              ttextInput("jFilter", "Journal", value="", groupClass="w-100"))
+            if(S$PGN$norecs) {           # Nothing in the catalog yet.
+               restOfPage = {
+                  tagList(
+                     bs4("r",
+                        bs4("c12", HTML0("<h5>No citations found</h5>")
                         )
+                     )
+                     # ),
+                     # ybox
+                  )
+               }
+            } else {
+               r = recGet(S$db, "catalog", c("catalogID", "title", "author", "journal", "Y", "reviewBest", "reviewCount"),
+                     tibble(c("catalogID", " IN ", paste0("(", paste0(S$PGN$chunkedIDs[[S$PGN$activePage]], collapse=","), ")"))))
+               if(S$P$Modify) {
+                  r$btn = paste0("<button id=cat_", r$catalogID, " class='btn border-dark btn-success'>Review</button>")
+               } else {
+                  r$btn = paste0("<button id=cat_", r$catalogID, " class='btn border-dark btn-primary'>View</button>")
+               }
+               restOfPage = tagList(
+                  uiOutput("showCatalog")
+               )
+               output$showCatalog <- renderUI({rv$render; isolate({
+                  tagList(
+                     bs4("r",
+                        bs4("c9",
+                           HTML("<span style='font-size: 1.25rem; color:#fff;'>Filter citations</span><br>"),
+                           ttextInput("abstract", "Phrase in title or abstract", value=S$fil$abstract, groupClass="w-75"),
+                           bs4("r",
+                              bs4("c5",
+                                 ttextInput("author", "An author", value=S$fil$author, groupClass="w-100")),
+                              bs4("c2",
+                                 ttextInput("year", "Year", value=S$fil$year, groupClass="w-100")),
+                              bs4("c5",
+                                 ttextInput("journal", "Journal", value=S$fil$journal, groupClass="w-100"))
+                           )
+                        ),
+                        bs4("c1"),
+                        bs4("c2",
+                           HTML("Review status<br>"),
+                           HTML(ifelse(S$fil$review_0, bs4("cbx", id="review_0", q="ck", "Not reviewed"), bs4("cbx", id="review_0", "Not reviewed"))),
+                           HTML(ifelse(S$fil$review_1, bs4("cbx", id="review_1", q="ck", "Stage 1 Fail"), bs4("cbx", id="review_1", "Stage 1 Fail"))),
+                           HTML(ifelse(S$fil$review_2, bs4("cbx", id="review_2", q="ck", "Stage 1 Pass"), bs4("cbx", id="review_2", "Stage 1 Pass"))),
+                           bs4("btn", id="filBTN_0", q="b", class="ml-4 mt-4", "Filter"))
                      ),
-                     bs4("c1"),
-                     bs4("c2",
-                        HTML("Review status<br>"),
-                        bs4("cbx", id="rFilter_0", q="ck", "Not reviewed"),
-                        bs4("cbx", id="rFilter_1", q="ck", "Stage 1 Fail"),
-                        bs4("cbx", id="rFilter_2", q="ck", "Stage 1 Pass"),
-                        bs4("btn", id="Filter_0", q="b", class="ml-4 mt-4", "Filter"))
-                  ),
-                  bs4("c12", bs4("hr0", class="pb-4")),
-                  bs4("r",
-                     bs4("c12", class="mb-2",
-                        HTML0("<span style='font-size: 1.25rem; color:#fff;'>", length(S$PGN$F$reviewBest), " results; ",
-                           sum(S$PGN$F$reviewBest==0), " not reviewed; ",
-                           sum(S$PGN$F$reviewBest==1), " failed; ",
-                           sum(S$PGN$F$reviewBest==2), " passed</span><br>")
-                  )),
-                  bs4("r",
-                     bs4("c12", HTML(S$PGN$start, S$PGN$middle, S$PGN$end),
-                     bs4("c12", bs4("hr0", class="pb-4"))
-                  )),
-                  bs4("r",
-                     bs4("c12",
-                         HTML(
-paste0("<div class='row justify-content-center'><div class='col-11'>",
+                     if(S$PGN$filteredIDs[1]>0) {
+                        tagList(
+                           bs4("c12", bs4("hr0", class="pb-4")),
+                           bs4("r",
+                              bs4("c12", class="mb-2",
+                                 HTML0("<span style='font-size: 1.15rem; color:#fff;'>",
+                                    format(length(S$PGN$FR$reviewBest), big.mark = ","), " results; ",
+                                    format(sum(S$PGN$FR$reviewBest==0), big.mark = ","), " not reviewed; ",
+                                    format(sum(S$PGN$FR$reviewBest==1), big.mark = ","), " failed; ",
+                                    format(sum(S$PGN$FR$reviewBest==2), big.mark = ","), " passed</span><br>")
+                           )),
+                           bs4("pgn", np=S$PGN$pageCount, ap=S$PGN$activePage),
+                           bs4("c12", bs4("hr0", class="pb-4")),
+                           bs4("r",
+                              bs4("c12",
+HTML(paste0("<div class='row justify-content-center'><div class='col-11'>",
        "<b>Review Status:</b> ", ifelse(r$reviewBest==0, "Not reviewed",
          ifelse(r$reviewBest==1, "Stage 1 Fail", "Stage 1 Pass")),
        " <b>Number of reviews:</b> ", r$reviewCount, "<br>",
@@ -164,16 +185,17 @@ paste0("<div class='row justify-content-center'><div class='col-11'>",
        "<b>Year:</b> ", r$Y, " <b>Journal:</b> ", r$journal,  "<br>",
        "</div>",
        "<div class='col-1'>", r$btn,
-       "</div>", bs4("c12", bs4("hr")), "</div>", collapse = "")
-                         )
-                  )),
-                  bs4("r",
-                     bs4("c12", HTML(S$PGN$start, S$PGN$middle, S$PGN$end)
-                  ))
-               )
-            })})
-         }
-      )
+       "</div>", bs4("c12", bs4("hr")), "</div>", collapse = ""))
+                           )),
+                           bs4("pgn", np=S$PGN$pageCount, ap=S$PGN$activePage)
+                        )
+                     } else {
+                        HTML("<h5>Nothing found</h5>")
+                     }
+                  )
+               })})
+            }
+         })
    reviewPageMenu = {
       if(S$hideMenus) {
          ""
@@ -202,11 +224,77 @@ observeEvent(input$js.omclick, {
    n  = uid[[1]][2]        #   to guarantee Shiny.onInputChange sees something new and returns it.
    switch(id,
       "sub" = {
-         rv$menuActive = n
+         rv$menuActive = as.numeric(n)
          rv$render = rv$render+1
+      },
+      "pgn" = {
+         S$PGN$activePage <<- as.numeric(n)
+         rv$render = rv$render+1
+      },
+      "filBTN" = {
+         S$fil$abstract <<- stripHTML(input$abstract)
+         S$fil$author   <<- stripHTML(input$author)
+         S$fil$year     <<- stripHTML(input$year)
+         S$fil$journal  <<- stripHTML(input$journal)
+         S$fil$review_0 <<- input$review_0
+         S$fil$review_1 <<- input$review_1
+         S$fil$review_2 <<- input$review_2
+         rs = sum(c(S$fil$review_0, S$fil$review_1, S$fil$review_2))
+         if(rs==0) {
+            S$modal_title <<- "Nothing found"
+            S$modal_text <<- HTML0("<p>You must check one or more of the <i>Review status</i> checkboxes. Unchecking them ",
+                                   "all will return no results.</p>")
+            S$modal_size <<- "m"
+            rv$modal_warning <- rv$modal_warning + 1
+         } else {
+            WHERE = tibble(dupOf=c("dupOf", "=", 0))
+            if(rs==1) {
+               if(S$fil$review_0) { WHERE$rBest = c("reviewBest", "=", 0) }
+               if(S$fil$review_1) { WHERE$rBest = c("reviewBest", "=", 1) }
+               if(S$fil$review_2) { WHERE$rBest = c("reviewBest", "=", 2) }
+            }
+            if(rs==2) {
+               if(S$fil$review_0 && S$fil$review_1) { WHERE$rBest = c("reviewBest", "!=", 2) }
+               if(S$fil$review_0 && S$fil$review_2) { WHERE$rBest = c("reviewBest", "!=", 1) }
+               if(S$fil$review_1 && S$fil$review_2) { WHERE$rBest = c("reviewBest", "!=", 0) }
+            }
+            if(S$fil$author!="") { WHERE$author=c("author", "LIKE", paste0("%", S$fil$author, "%"))}
+            if(S$fil$year!="") { WHERE$year=c("Y", "LIKE", paste0("%", S$fil$year, "%"))}
+            if(S$fil$journal!="") { WHERE$journal=c("journal", "LIKE", paste0("%", S$fil$journal, "%"))}
+            dbLink <- poolCheckout(shiny.pool)                                        # get a dbLink from the pool
+            wherePairs = wherez(WHERE, dbLink)
+            if(S$fil$abstract!="") {                                                  # all this for an OR
+               qabs = dbQuoteString(dbLink, paste0("%", S$fil$abstract, "%"))
+               wherePairs = paste0(wherePairs, " AND (`abstract` LIKE ", qabs, " OR `title` LIKE ", qabs, ")")
+            }
+            selects = dbQuoteIdentifier(dbLink, c("catalogID", "reviewBest"))
+            selects = paste0(selects, collapse=",")
+            QUERY = paste0("SELECT ", selects, " FROM ", dbt(S$db, "catalog", dbLink), " WHERE ", wherePairs, ";")
+            S$PGN$FR <<- dbGetQuery(dbLink, QUERY)                                    # perform raw SQL Query
+            poolReturn(dbLink)                                                        # return dbLink
+            S$PGN$filteredIDs <<- S$PGN$FR$catalogID
+            S$PGN$chunkedIDs <<- chunker(S$PGN$filteredIDs, 30)
+            S$PGN$pageCount <<- length(S$PGN$chunkedIDs)
+            S$PGN$activePage <<- 1
+            rv$render = rv$render+1
+         }
       },
       message(paste0("In input$js.omclick observer, no handler for ", id, "."))
    )
 }, ignoreNULL = TRUE, ignoreInit = TRUE)
+
+
+wherezzz = function(WHERE, dbLink) {   # WHERE is a tibble
+   w = ""
+   for(i in 1:ncol(WHERE)) {                                  # Need all this to keep from putting quotes on numbers
+      if(WHERE[[2,1]]!=" IN " && suppressWarnings(is.na(as.numeric(WHERE[[3,i]])))) { # An NA means it's a string
+         v = dbQuoteString(dbLink, WHERE[[3,i]])
+      } else {
+         v = WHERE[[3,i]]
+      }
+      w[i] = paste0(dbQuoteIdentifier(dbLink, WHERE[[1,i]]), WHERE[[2,i]], v)
+   }
+   return(paste0(w, collapse = " AND "))
+}
 
 
