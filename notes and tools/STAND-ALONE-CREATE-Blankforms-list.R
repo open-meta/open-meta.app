e0cb8ebc94b9a98be2f35312c3d2a5f6c29d4d04
Tom Weishaar <tom.weishaar@gmail.com>
2018-09-20 16:44:39 -0400

    Blankforms support for inputMetaAux.R
    And other cleanup.

--- notes and tools/STAND-ALONE-CREATE-Blankforms-list.R
+++ notes and tools/STAND-ALONE-CREATE-Blankforms-list.R
@@ -6,225 +6,207 @@
 
 library(tibble)
 
-imGetBlankFORMrow = function(type) {
-   return(tibble(
-      type=type,                # see S$IN$codeTypes
-      id="",                    # identifies one of these parameter for Forms; otherwise paste0("id", paste0(utf8ToInt(name), collapse=""))
-      name="",                  #    a short identifier for tables and figures (must be unique)
-      label="",
-      width="",                 # for width the possible options are 25%-50%-75%-100%
-      value="",                 # selected option(s) for select-radio-checkbox; otherwise current value
-      placeholder="",
-      maxlength="",             # Number of characters accepted
-      min="",                   # These three are for the numeric input type
-      max="",                   #  ...the value of these numbers will be in strings, however
-      step="",
-      options="",               # all the options for select-radio-checkbox, separated by ";"
-                                # for "form", the possible options are: inline, sameline, disabled, and locked
-      inline=TRUE,              # whether radio buttons and checkboxes are inline or in a column
-      sameline=FALSE,           # whether the current input should be on the same line as the previous input (if it would fit)
-      disabled=FALSE,           # whether this input should be disabled
-      locked=FALSE,             # whether this FORMrow should be locked
-      order=999                 # used to reorder the inputs
-#      required=,               # requires a true form submit to work, which we don't do
-#      readonly=,               # use disabled, which won't accept focus like readonly will
-#      size=0,                  # width of input, but form-control class overrides this one
-   ))
-}
+source("app/inputMetaAux.R", local=TRUE)  # load the shared imGetBlankFORMrow function
 
 imBlankforms <- list()         # This is a list of tibbles; one multi-row tibble for each type of input
 
+imName <- imGetBlankFORMrow("text")
+   imName$id <- "name"
+   imName$label <- "Short name"
+   imName$placeholder <- "Required..."
+   imName$maxlength <- "15"
+   imName$width <- "25%"
+   imName$helptext <- "Used in tables and graphs.<br>Must be unique; can't be changed.<br>15 characters maximum."
+
+imLabel <- imGetBlankFORMrow("text")
+   imLabel$id <- "label"
+   imLabel$label <- "Long name"
+   imLabel$placeholder <- "Required..."
+   imLabel$helptext <- "Used in forms."
+   imLabel$width <- "75%"
+
+imHelptext <- imGetBlankFORMrow("text")
+   imHelptext$id <- "helptext"
+   imHelptext$label <- "Help text"
+   imHelptext$helptext <- "This is what help text looks like."
+   imHelptext$width <- "75%"
+
+imWidth <- imGetBlankFORMrow("radio")
+   imWidth$id <- "width"
+   imWidth$label <- "Width of this input"
+   imWidth$options <- "25%; 33%; 50%; 66%; 75%; 100%"
+   imWidth$value <- "100%"
+   imWidth$inline = TRUE
+   imWidth$width <- "50%"
+
+imOther1 <- imGetBlankFORMrow("checkbox")            # no disable (of entire widget)
+   imOther1$id <- "other"                            #   used by quill, radio, and checkbox
+   imOther1$label <- "Other options"                 #   Note: HTML allows individual radio/checkbox options to
+   imOther1$options <- "sameline"                    #      be disabled, but we don't support that
+   imOther1$inline = TRUE
+   imOther1$sameline <- TRUE
+   imOther1$width <- "50%"
+
+imOther2 <- imGetBlankFORMrow("checkbox")            # options include disabled
+   imOther2$id <- "other"                            #    used by text, numeric, and select
+   imOther2$label <- "Other options"
+   imOther2$options <- "sameline; disabled"
+   imOther2$inline = TRUE
+   imOther2$sameline <- TRUE
+   imOther2$width <- "50%"
+
+
 ### Simple Text input
 w=1
-   r <- imGetBlankFORMrow("text")
-   r$id[w] <- "name"
-   r$label[w] <- "Short name for this input's data in tables and graphs (must be unique)"
-   r$placeholder[w] <- "15 chars max"
-   r$maxlength[w] <- "15"
-   r$width[w] <- "25%"
+   r <- imName
 w=w+1
-   r[w,] <- imGetBlankFORMrow("text")
-   r$id[w] <- "label"
-   r$label[w] <- "Long name for forms"
+   r[w,] <- imLabel
+w=w+1
+   r[w,] <- imHelptext
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "value"
    r$label[w] <- "Initial value for this input"
-   r$placeholder[w] <- "This is placeholder text. Leave this initial value blank if you enter placeholder text next..."
+   r$width[w] <- "75%"
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "placeholder"
    r$label[w] <- "Placeholder text for this input"
+   r$placeholder[w] <- "This is placeholder text. It appears only when the value is blank..."
+   r$width[w] <- "75%"
 w=w+1
    r[w,] <- imGetBlankFORMrow("number")
    r$id[w] <- "maxlength"
-   r$label[w] <- "Maximum number of characters allowed"
+   r$label[w] <- "Maximum characters allowed"
    r$placeholder[w] <- "Blank means no maximum..."
-   r$width[w] <- "25%"
+   r$width[w] <- "33%"
 w=w+1
-   r[w,] <- imGetBlankFORMrow("radio")
-   r$id[w] <- "width"
-   r$label[w] <- "Width of this input"
-   r$options[w] <- "25%; 50%; 75%; 100%"
-   r$value[w] <- "100%"
-   r$inline[w] = TRUE
+   r[w,] <- imWidth
 w=w+1
-   r[w,] <- imGetBlankFORMrow("checkbox")
-   r$id[w] <- "other"
-   r$label[w] <- "Other options"
-   r$options[w] <- "sameline; disabled"
-   r$inline[w] = TRUE
+   r[w,] <- imOther2
 imBlankforms[["text"]] <- r          # userType is "Simple Text"
 
 ### Text Editor input
 w=1
-   r <- imGetBlankFORMrow("text")
-   r$id[w] <- "name"
-   r$label[w] <- "Short name for this input's data in tables and graphs (must be unique)"
-   r$placeholder[w] <- "15 chars max"
-   r$maxlength[w] <- "15"
-   r$width[w] <- "25%"
+   r <- imName
 w=w+1
-   r[w,] <- imGetBlankFORMrow("text")
-   r$id[w] <- "label"
-   r$label[w] <- "Label for the editor"
+   r[w,] <- imLabel
+w=w+1
+   r[w,] <- imHelptext
 w=w+1
    r[w,] <- imGetBlankFORMrow("quill")
    r$id[w] <- "value"
    r$label[w] <- "Any initial text for the editor"
-   r$placeholder[w] <- "This is placeholder text. Leave this initial text blank if you want to enter placeholder text next..."
+   r$width[w] <- "75%"
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "placeholder"
    r$label[w] <- "Placeholder text for the editor"
+   r$placeholder[w] <- "This is placeholder text. It appears only when the editor is empty..."
+   r$width[w] <- "75%"
 w=w+1
-   r[w,] <- imGetBlankFORMrow("checkbox")
-   r$id[w] <- "other"
-   r$label[w] <- "Other options"
-   r$options[w] <- ""
-   r$inline[w] = TRUE
+   r[w,] <- imWidth
+w=w+1
+   r[w,] <- imOther1
 imBlankforms[["quill"]] <- r         # userType is "Text editor"
 
 ### Numeric input
 w=1
-   r <- imGetBlankFORMrow("text")
-   r$id[w] <- "name"
-   r$label[w] <- "Short name for this input's data in tables and graphs (must be unique)"
-   r$placeholder[w] <- "15 chars max"
-   r$maxlength[w] <- "15"
-   r$width[w] <- "25%"
+   r <- imName
 w=w+1
-   r[w,] <- imGetBlankFORMrow("text")
-   r$id[w] <- "label"
-   r$label[w] <- "Label for this input"
+   r[w,] <- imLabel
+w=w+1
+   r[w,] <- imHelptext
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "value"
    r$label[w] <- "Initial value for this input"
-   r$placeholder[w] <- "This is placeholder text. Leave this initial value blank if you enter placeholder text next..."
+   r$placeholder[w] <- "Blank means no initial value..."
+   r$width[w] <- "33%"
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "placeholder"
    r$label[w] <- "Placeholder text for this input"
+   r$width[w] <- "33%"
+   r$sameline[w] <- TRUE
 w=w+1
    r[w,] <- imGetBlankFORMrow("number")
    r$id[w] <- "min"
    r$label[w] <- "Minimum"
    r$placeholder[w] <- "Blank means no minimum..."
-   r$width[w] <- "25%"
+   r$helptext[w] <- "Smallest acceptable number."
+   r$width[w] <- "33%"
 w=w+1
    r[w,] <- imGetBlankFORMrow("number")
    r$id[w] <- "max"
    r$label[w] <- "Maximum"
    r$placeholder[w] <- "Blank means no maximum..."
-   r$width[w] <- "25%"
+   r$helptext[w] <- "Largest acceptable number."
+   r$width[w] <- "33%"
+   r$sameline[w] <- TRUE
 w=w+1
    r[w,] <- imGetBlankFORMrow("number")
    r$id[w] <- "step"
    r$label[w] <- "Step"
    r$placeholder[w] <- "Blank means no step..."
-   r$width[w] <- "25%"
+   r$helptext[w] <- "Up/Down arrow increment/decrement."
+   r$width[w] <- "33%"
+   r$sameline[w] <- TRUE
 w=w+1
-   r[w,] <- imGetBlankFORMrow("radio")
-   r$id[w] <- "width"
-   r$label[w] <- "Width of this input"
-   r$options[w] <- "25%; 50%; 75%; 100%"
-   r$value[w] <- "25%"
-   r$inline[w] = TRUE
+   r[w,] <- imWidth
 w=w+1
-   r[w,] <- imGetBlankFORMrow("checkbox")
-   r$id[w] <- "other"
-   r$label[w] <- "Other options"
-   r$options[w] <- "sameline; disabled"
-   r$inline[w] = TRUE
+   r[w,] <- imOther2
 imBlankforms[["number"]] <- r         # userType is "Numeric"
 
 ### Select input
 w=1
-   r <- imGetBlankFORMrow("text")
-   r$id[w] <- "name"
-   r$label[w] <- "Short name for this input's data in tables and graphs (must be unique)"
-   r$placeholder[w] <- "15 chars max"
-   r$maxlength[w] <- "15"
-   r$width[w] <- "25%"
+   r <- imName
 w=w+1
-   r[w,] <- imGetBlankFORMrow("text")
-   r$id[w] <- "label"
-   r$label[w] <- "Label for this input"
+   r[w,] <- imLabel
+w=w+1
+   r[w,] <- imHelptext
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "options"
-   r$label[w] <- "Options for this input"
-   r$placeholder[w] <- "Put a semicolon between options..."
+   r$label[w] <- "Options for this dropdown"
+   r$placeholder[w] <- "Required..."
+   r$helptext[w] <- "Put exactly one semicolon between options."
+   r$width[w] <- "66%"
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "value"
    r$label[w] <- "Selected option"
-   r$width[w] <- "25%"
+   r$helptext[w] <- "Only one option can be selected.<br>Text must exactly match the option name."
+   r$width[w] <- "33%"
+   r$sameline[w] <- TRUE
 w=w+1
-   r[w,] <- imGetBlankFORMrow("radio")
-   r$id[w] <- "width"
-   r$label[w] <- "Width of this input"
-   r$options[w] <- "25%; 50%; 75%; 100%"
-   r$value[w] <- "100%"
-   r$inline[w] = TRUE
+   r[w,] <- imWidth
 w=w+1
-   r[w,] <- imGetBlankFORMrow("checkbox")
-   r$id[w] <- "other"
-   r$label[w] <- "Other options"
-   r$options[w] <- "sameline; disabled"
-   r$inline[w] = TRUE
+   r[w,] <- imOther2
 imBlankforms[["select"]] <- r         # userType is "Select (dropdown)"
 
 ### Radio button input
 w=1
-   r <- imGetBlankFORMrow("text")
-   r$id[w] <- "name"
-   r$label[w] <- "Short name for this input's data in tables and graphs (must be unique)"
-   r$placeholder[w] <- "15 chars max"
-   r$maxlength[w] <- "15"
-   r$width[w] <- "25%"
+   r <- imName
 w=w+1
-   r[w,] <- imGetBlankFORMrow("text")
-   r$id[w] <- "label"
-   r$label[w] <- "Label for this input"
+   r[w,] <- imLabel
+w=w+1
+   r[w,] <- imHelptext
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "options"
-   r$label[w] <- "Options for this input"
-   r$placeholder[w] <- "Put a semicolon between options..."
+   r$label[w] <- "Radio button names"
+   r$placeholder[w] <- "Required..."
+   r$helptext[w] <- "Put exactly one semicolon between radio button names."
+   r$width[w] <- "66%"
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "value"
-   r$label[w] <- "Selected option"
-   r$width[w] <- "25%"
-w=w+1
-   r[w,] <- imGetBlankFORMrow("radio")
-   r$id[w] <- "width"
-   r$label[w] <- "Width of this input"
-   r$options[w] <- "25%; 50%; 75%; 100%"
-   r$value[w] <- "100%"
-   r$inline[w] = TRUE
+   r$label[w] <- "Selected radio button"
+   r$placeholder[w] <- "Required..."
+   r$helptext[w] <- "One and only one radio button must be selected.<br>Text must exactly match the button name."
+   r$width[w] <- "33%"
+   r$sameline[w] <- TRUE
 w=w+1
    r[w,] <- imGetBlankFORMrow("radio")
    r$id[w] <- "direction"
@@ -233,43 +215,32 @@ w=w+1
    r$value[w] <- "across the page"
    r$inline[w] = TRUE
 w=w+1
-   r[w,] <- imGetBlankFORMrow("checkbox")
-   r$id[w] <- "other"
-   r$label[w] <- "Other options"
-   r$options[w] <- "sameline; disabled"
-   r$inline[w] = TRUE
+   r[w,] <- imWidth
+w=w+1
+   r[w,] <- imOther1
 imBlankforms[["radio"]] <- r         # userType is "Radio buttons"
 
 ### Checkboxes input
 w=1
-   r <- imGetBlankFORMrow("text")
-   r$id[w] <- "name"
-   r$label[w] <- "Short name for this input's data in tables and graphs (must be unique)"
-   r$placeholder[w] <- "15 chars max"
-   r$maxlength[w] <- "15"
-   r$width[w] <- "25%"
+   r <- imName
 w=w+1
-   r[w,] <- imGetBlankFORMrow("text")
-   r$id[w] <- "label"
-   r$label[w] <- "Label for this input"
+   r[w,] <- imLabel
+w=w+1
+   r[w,] <- imHelptext
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "options"
-   r$label[w] <- "Options for this input"
-   r$placeholder[w] <- "Put a semicolon between options..."
+   r$label[w] <- "A name for each checkbox"
+   r$placeholder[w] <- "Required..."
+   r$helptext[w] <- "Minimum is one checkbox name.<br>No maximum.<br>Put exactly one semicolon between checkbox names."
+   r$width[w] <- "66%"
 w=w+1
    r[w,] <- imGetBlankFORMrow("text")
    r$id[w] <- "value"
-   r$label[w] <- "Selected options"
-   r$width[w] <- "50%"
-   r$placeholder[w] <- "If more than one, put a semicolon between them..."
-w=w+1
-   r[w,] <- imGetBlankFORMrow("radio")
-   r$id[w] <- "width"
-   r$label[w] <- "Width of this input"
-   r$options[w] <- "25%; 50%; 75%; 100%"
-   r$value[w] <- "100%"
-   r$inline[w] = TRUE
+   r$label[w] <- "Checked checkboxes"
+   r$helptext[w] <- "Zero to all checkboxes can be checked.<br>Put exactly one semicolon between them.<br>Text must exactly match the checkbox name."
+   r$width[w] <- "33%"
+   r$sameline[w] <- TRUE
 w=w+1
    r[w,] <- imGetBlankFORMrow("radio")
    r$id[w] <- "direction"
@@ -278,11 +249,9 @@ w=w+1
    r$value[w] <- "across the page"
    r$inline[w] = TRUE
 w=w+1
-   r[w,] <- imGetBlankFORMrow("checkbox")
-   r$id[w] <- "other"
-   r$label[w] <- "Other options"
-   r$options[w] <- "sameline; disabled"
-   r$inline[w] = TRUE
+   r[w,] <- imWidth
+w=w+1
+   r[w,] <- imOther1
 imBlankforms[["checkbox"]] <- r         # userType is "Checkboxes"
 
 ### TF_Chex input
@@ -335,7 +304,7 @@ saveRDS(imBlankforms, file="app/Blankforms.RDS")
 ##################################################################################
 
 
-rm(r, w, imBlankforms, imGetBlankFORMrow)
+rm(r, w, imBlankforms, imGetBlankFORMrow, imName, imLabel, imHelptext, imWidth, imOther)
 
 
 #imBlankforms <- readRDS(file="app/Blankforms.RDS")
