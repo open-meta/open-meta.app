bd222fec77aef1e8e995a142a6098c3ee986d28e
Tom Weishaar <tom.weishaar@gmail.com>
2018-09-22 17:32:49 -0400

    Add/Edit Outcomes

--- app/Extract.R
+++ app/Extract.R
@@ -12,7 +12,10 @@ source("chokidar.R", local=TRUE)
 #   * if(S$P$Msg=="") the user has permission to see the page and you can render the Meat
 #   * S$P$Modify  - whether the user has permission to modify things on this page
 
-S$OC$NUM = 0
+# load the inputMeta.R code (also used by other pages)
+source("inputMeta.R", local=TRUE)
+
+S$PGN$itemsPerPage <- 30       # Is this the only pagination thing we need???
 
 rv$menuActive = 1    # Start out on first sub-menu
 
@@ -28,49 +31,76 @@ if(S$P$Msg=="") {
             restOfPage = tagList(
                bs4("r", bs4("c1"), bs4("c10",
                   uiOutput("addOutcome"),
-                  DTOutput("showOutcomes"),
+                  uiOutput("showOutcomes"),
                   uiOutput("yboxOutcomes")))
             )
-            output$addOutcome = renderUI(tagList(
-               bs4("r", bs4("c9",
-                  HTML0("Outcome Name<br>"),
-                  textInput("outcomeName", label=NULL, value="", width="100%")),
-               bs4("c3", align="sc", bs4("btn", uid="new_0", q="g", class="mt-1", "Add Outcome"))),
+            output$addOutcome <- renderUI(tagList(
+               bs4("btn", id="addInput", q="g", "Add an Outcome"),
                bs4("hr")
-
-         ))
-            output$showOutcomes  <- renderDT({
-               Rx = omRx(                 # Get project's outcomes
-                  db = S$db,
-                  table = "outcome",
-                  SELECT = c("outcomeNUM", "value"),
-                  WHERE = tibble(c("name", "=", "outcomeName")),
-                  buttons = list(view=list(id="viewOutcome", label="View", q="b", class=""),
-                                 edit=list(id="editOutcome", label="Edit", q="g", class=""))
-               )
-               # Adjust which buttons will show in table
-               if(nrow(Rx)>0) {                     # Skip this if nothing was returned.
-                  if(!S$P$Modify) {                    # Without permission to modify all you can do is View
-                     Rx = Rx[-4]                       #    delete all button columns but View
-                  } else {
-                     Rx = Rx[-3]                    # Remove View buttons
-                  }
-                  Rx <- Rx[,-1]                     # Remove ID column
-               }
-               ###
-               omDT(Rx,
+            ))
+            S$PGN$activePage <<- 1
+            output$showOutcomes  <- renderUI({
+               noResultsMsg <- "No Outcomes have been added to this project yet."
+               TABLE = "outcome"
+               WHERE = tibble(c("name", "=", "Outcome"))
+               R <- recGet(S$db, TABLE, paste0(TABLE,"ID"), WHERE)             # Get all record IDs
+               if(R[1,1]==0) {
+                  S$PGN$flag$firstOne <<- TRUE
+                  tagList(
+                     bs4("r",
+                    #    bs4("c12", bs4("hr0", class="pb-4")),
+                        bs4("c12", HTML0("<h5>", noResultsMsg, "</h5>")),
+                        bs4("c12", bs4("hr0", class="pb-4"))
+                     )
+                  )
+               } else {
+                  S$PGN$flag$firstOne <<- FALSE
+                  IDs <- as.integer(R$outcomeID)
+                  chunkedIDs <- chunker(IDs, S$PGN$itemsPerPage)
+                  pageCount <- length(chunkedIDs)
+                  SELECT = c("value")
+                  R = recGet(S$db, TABLE, SELECT, tibble(c("outcomeID", " IN ",
+                        paste0("(", paste0(chunkedIDs[[S$PGN$activePage]], collapse=","), ")"))))
                   if(S$P$Modify) {
-                     cnames = c("Outcome Name", "Edit Details")
+                     btnid = "editOutcome"
+                     btnq = "g"
+                     btnlabel = "Edit"
                   } else {
-                     cnames = c("Outcome Name", "View Details")
-                  },
-                  colesc = c(1),        # columns to escape (minus means don't escape)
-                  noText = "No outcomes found for this project"      # What to say when there are no results
-               )
-            },
-            # renderDT() parameters go here:
-            server = FALSE
-            )
+                     btnid = "viewOutcome"
+                     btnq = "b"
+                     btnlabel = "View"
+                  }
+                  pattern = rep("XxX", nrow(R))
+                  btn = bs4("btn", id=btnid, n="XxX", q=btnq, btnlabel)
+                  R[,"btn"] = str_replace_all(btn, pattern, as.character(chunkedIDs[[S$PGN$activePage]]))   # str_replace is vectorized
+                  tagList(
+                     bs4("r",
+                        bs4("c12", bs4("hr0", class="pb-4")),
+                        bs4("pgn", np=pageCount, ap=S$PGN$activePage),
+                        bs4("c12", bs4("hr0", class="pb-4"))
+                     ),
+# This does the entire table with one vectorized paste0(). R is a tibble and its columns are vectors.
+#    The "collapse" at the end creates one long string. R[3] is the value column and R[4] is the button column.
+# In this particular example, there's one row with a col-11 containing all the data, using <br> to start new
+#    lines, and col-1 for the button. Note that cites$btn isn't stored in MySQL, but is added to "cites" above.
+HTML(paste0(
+'<div class="row justify-content-center">
+   <div class="col-11">
+      ', R$value,'
+   </div>
+   <div class="col-1">
+      ', R$btn, '
+   </div>
+   ', bs4('c12', bs4('hr')), '
+</div>', collapse = '')),     # End of paste0()
+                     bs4("r",
+#                        bs4("c12", bs4("hr0", class="pb-4")),       # last row of table provides this <hr>
+                        bs4("pgn", np=pageCount, ap=S$PGN$activePage),
+                        bs4("c12", bs4("hr0", class="pb-4"))
+                     )
+                  )
+               }
+            })  # end of render
             output$yboxOutcomes = renderUI(tagList(
                bs4("r", class="mt-3", bs4("c12", bs4("cd", q="y", bs4("cdb", bs4("cdt", HTML0(
 "<p>If you are a member of this project with appropriate permissions, you can </p>
@@ -80,17 +110,24 @@ if(S$P$Msg=="") {
          "3" = {
             restOfPage = "Trials-Arms-Groups Here"
          },
-         "4" = {
-            loopR = ""
-            for(i in 1:nrow(S$OC$data)) {
-               loopR = paste0(loopR, bs4(S$OC$data$widget[i], id=S$OC$data$id[i], ck=S$OC$data$value[i], S$OC$data$label[i]))
+         "10" = {                       # edit Outcome
+            TABLE = "outcome"
+            S$PGN$FORM <<- imGetFORM(TABLE)
+            NUM <- ID2NUM(TABLE, S$PGN$outcomeID)
+            R <- recGet(S$db, TABLE, c("name", "value"), tibble(c(paste0(TABLE,"NUM"), "=", NUM)))
+            if(!S$P$Modify) {
+               S$PGN$FORM$disable <<- TRUE
             }
+            for(i in 1:nrow(S$PGN$FORM)) {
+               S$PGN$FORM$value[i] <<- R[R$name==S$PGN$FORM$name[i], "value"]
+            }
+            print(S$PGN$FORM$disable)
+            # If outcome exists, load form with current values
             restOfPage = tagList(
-               HTML("<pre>", S$OC$data, "</pre>"),
-               HTML(loopR),
+               imForm2HTML(S$PGN$FORM),
                bs4("d", class="text-right mt-3",
                   bs4("btn", id="cancel", n=1, q="b", "Cancel"),
-                  bs4("btn", id="save", n=1, q="b", "Save Outcome Details")
+                  ifelse(S$P$Modify, bs4("btn", id="save", n=1, q="b", "Save Outcome Details"), "")
                )
             )
          }
@@ -100,7 +137,7 @@ if(S$P$Msg=="") {
       # $('#tinid')[0].value gets what's in a text input
       # $('#cbxid')[0].checked gets status of a checkbox
 
-
+      # better yet, use input[[dynamicName]] for input$staticName
 
       pageMenu = {
          if(S$hideMenus) {
@@ -135,32 +172,37 @@ observeEvent(input$js.omclick, {
          rv$menuActive = n
          rv$limn = rv$limn+1
       },
-      "new" = {
-         if(input$outcomeName=="") {
-            S$modal_title <<- "Name required."
-            S$modal_text <<- HTML("<p>Please provide a name for the new outcome.</p>")
-            S$modal_size <<- "s"
-            rv$modal_warning <- rv$modal_warning + 1
-         } else {
-            outcome <- recGet(S$db, "outcome", SELECT="", WHERE="")
-            outcome$name[2]  <- "outcomeName"
-            outcome$value[2] <- htmlEscape(input$outcomeName)
-            outcome <- recSave(outcome, S$db)                # At this point, outcomeNUM is always zero;
-            outcome$outcomeNUM[2] <- outcome$outcomeID[2]    #   Change it to outcomeID of this initial record
-            outcome <- recSave(outcome, S$db)
-            rv$limn = rv$limn+1
-         }
+      "addInput" = {
+    #     S$PGN$flag$editing <<- FALSE
+         S$PGN$outcomeID <<- 0
+         S$hideMenus <<- TRUE
+         rv$menuActive = 10
+         rv$limn = rv$limn+1
       },
       "editOutcome" = {
-         S$OC$NUM <<- n
-         S$OC$data <<- recGet(S$db, "outcome", SELECT=c("outcomeNUM", "name", "value"), WHERE=tibble(c("outcomeNUM", "=", S$OC$NUM)))
-         S$OC$data <<- tibble(widget=c("tin", "cbx"), id=c("tinid", "cbxid"), label=c("text", "checkbox"),
-                              value=c("", "TRUE"))
+     #    S$PGN$flag$editing <<- TRUE
+         S$PGN$outcomeID <<- n
          S$hideMenus <<- TRUE
-         rv$menuActive = 4
+         rv$menuActive = 10
+         rv$limn = rv$limn+1
+      },
+      "viewOutcome" = {
+         S$PGN$outcomeID <<- n
+         S$hideMenus <<- FALSE
+         rv$menuActive = 10
          rv$limn = rv$limn+1
       },
       "save" = {
+         NUM <- ID2NUM(t="outcome", ID=S$PGN$outcomeID)
+         for(i in 1:nrow(S$PGN$FORM)) {
+            R <-  recGet(S$db, "outcome", SELECT="**", WHERE=tibble(
+               c("outcomeNUM", "=", NUM), c("name", "=", S$PGN$FORM$name[i])))
+            R$outcomeNUM[2] = NUM
+            R$name[2] = S$PGN$FORM$name[i]
+            R$value[2] = str_trim(stripHTML(as.character(input[[S$PGN$FORM$id[i]]])))
+            R <- recSave(R, S$db)
+            # What about quill?
+         }
          S$hideMenus <<- FALSE
          rv$menuActive = 2
          rv$limn = rv$limn+1
@@ -179,4 +221,21 @@ observeEvent(input$js.omclick, {
    )
 }, ignoreNULL = TRUE, ignoreInit = TRUE)
 
+ID2NUM <- function(t, ID) {
+   if(S$PGN$flag$firstOne) {        # first outcome, use 1
+      return(1)
+   } else {
+      TABLE = t
+      SELECT = paste0(t,"NUM")
+      tableID = paste0(t,"ID")
+      if(ID>0) {                     # already exists, get it
+         R <- recGet(S$db, TABLE, SELECT, WHERE=tibble(c(tableID, "=", ID)))
+         return(R$outcomeNUM)
+      } else {                       # not first: get max of outcomeNUMs and add 1
+         R <- recGet(S$db, TABLE, SELECT, WHERE=tibble(c(tableID, ">", 0)))
+         return(max(as.numeric(R$outcomeNUM)) + 1)
+      }
+   }
+
+}
 
