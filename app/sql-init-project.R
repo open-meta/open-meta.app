7639e09d273491c1266fd1565a16e79f82e94a36
Tom Weishaar <tom.weishaar@gmail.com>
2018-10-11 11:08:52 -0400

    Add creation of trial table

--- app/sql-init-project.R
+++ app/sql-init-project.R
@@ -29,6 +29,7 @@ initProject = function(projectID, projectName, pool) {
    r = dbExecute(dbLink, createTable(S$db, "pico"))
    r = dbExecute(dbLink, createTable(S$db, "extract"))
    r = dbExecute(dbLink, createTable(S$db, "ids"))
+   r = dbExecute(dbLink, createTable(S$db, "trial"))
 
 ### Membership table is done by caller on both new installs and new projects
 
