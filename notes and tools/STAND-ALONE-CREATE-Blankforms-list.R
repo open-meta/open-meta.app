f23bf7edccce6dbf549a61b783fd693917c8b873
Tom Weishaar <tom.weishaar@gmail.com>
2018-11-29 21:40:52 -0500

    Revert Blankforms2.RDS to Blankforms.RDS

--- notes and tools/STAND-ALONE-CREATE-Blankforms-list.R
+++ notes and tools/STAND-ALONE-CREATE-Blankforms-list.R
@@ -1,12 +1,12 @@
 ### open-meta STAND-ALONE-Blankforms-list.R
-### Tom Weishaar - Sep 2018 - v0.1
+### Tom Weishaar - Nov 2018 - v0.2
 ### This is a stand-alone file for creating a list of Blankform defintions.
 ###    This list is saved to a file and loaded during app startup.
 ###    The list is used to create customizable forms.
 
 library(tibble)
 
-source("app/inputMetaAux.R", local=TRUE)  # load the shared imGetBlankFORMrow function
+source("app/imGetBlankFORMrow.R", local=TRUE)  # load the shared imGetBlankFORMrow function
 
 imBlankforms <- list()         # This is a list of tibbles; one multi-row tibble for each type of input
 
@@ -34,6 +34,15 @@ imColumn <- imGetBlankFORMrow("text")
    imColumn$width <- "25%"
    imColumn$helptext <- "Name of the table column that data from this field will be stored in."
 
+imColor <- imGetBlankFORMrow("select")
+   imColor$id <- "color"
+   imColor$label <- "Color"
+   imColor$sameline <- TRUE
+   imColor$width <- "25%"
+   imColor$options <- "default;blue;green;purple;yellow;red"
+   imColor$value <- "default"
+   imColor$helptext <- "Background color of this input."
+
 imLabel <- imGetBlankFORMrow("text")
    imLabel$id <- "label"
    imLabel$label <- "Long name"
@@ -47,21 +56,20 @@ imHelptext <- imGetBlankFORMrow("text")
    imHelptext$helptext <- "This is what help text looks like."
    imHelptext$width <- "75%"
 
-imWidth <- imGetBlankFORMrow("radio")
+imWidth <- imGetBlankFORMrow("text")
    imWidth$id <- "width"
    imWidth$label <- "Width of this input"
-   imWidth$options <- "25%; 33%; 50%; 66%; 75%; 100%"
-   imWidth$value <- "100%"
-   imWidth$inline = TRUE
-   imWidth$width <- "50%"
+   imWidth$placeholder <- "Blank means 100%..."
+   imWidth$helptext <- "Width in rems or 25%-33%-50%-66%-75%-100%"
+   imWidth$width <- "33%"
 
 imOther <- imGetBlankFORMrow("checkbox")            # options include disabled
    imOther$id <- "other"                            #    used by text, numeric, and select
    imOther$label <- "Other options"
    imOther$options <- "sameline; disabled"
-   imOther$inline = TRUE
    imOther$sameline <- TRUE
    imOther$width <- "50%"
+   imOther$inline = TRUE
 
 
 ### Simple Text input
@@ -71,6 +79,8 @@ w=w+1
    r[w,] <- imTable
 w=w+1
    r[w,] <- imColumn
+w=w+1
+   r[w,] <- imColor
 w=w+1
    r[w,] <- imLabel
 w=w+1
@@ -134,6 +144,8 @@ w=w+1
 w=w+1
    r[w,] <- imColumn
 w=w+1
+   r[w,] <- imColor
+w=w+1
    r[w,] <- imLabel
 w=w+1
    r[w,] <- imHelptext
@@ -185,6 +197,8 @@ w=w+1
    r[w,] <- imTable
 w=w+1
    r[w,] <- imColumn
+w=w+1
+   r[w,] <- imColor
 w=w+1
    r[w,] <- imLabel
 w=w+1
@@ -286,6 +300,118 @@ w=w+1
    r[w,] <- imOther
 imBlankforms[["checkbox"]] <- r         # userType is "Checkboxes"
 
+### Spacer
+w=1
+   r <- imGetBlankFORMrow("text")
+   r[w,] <- imColor
+w=w+1
+   r[w,] <- imGetBlankFORMrow("text")
+   r$id[w] <- "label"
+   r$label[w] <- "Text for spacer, if any"
+   r$width[w] <- "75%"
+w=w+1
+   r[w,] <- imGetBlankFORMrow("text")
+   r$id[w] <- "width"
+   r$label[w] <- "Width of the spacer"
+   r$placeholder[w] <- "Blank means 100%..."
+   r$helptext[w] <- "Width in rems or 25%-33%-50%-66%-75%-100%"
+   r$width[w] <- "33%"
+w=w+1
+   r[w,] <- imGetBlankFORMrow("radio")
+   r$id[w] <- "placeholder"
+   r$label[w] <- "Position of text in the &lt;div&gt;"
+   r$options[w] <- "Left;Center;Right"
+   r$value[w] <- "Center"
+   r$sameline[w] <- FALSE
+   r$inline[w] = TRUE
+w=w+1
+   r[w,] <- imGetBlankFORMrow("text")
+   r$id[w] <- "value"
+   r$label[w] <- "Height of the spacer"
+   r$placeholder[w] <- "Blank means 1rem..."
+   r$helptext[w] <- "Height in rems; 0 to 1.3 are all 29px"
+   r$sameline[w] <- TRUE
+   r$width[w] <- "33%"
+   r$sameline[w] <- FALSE
+w=w+1
+   r[w,] <- imGetBlankFORMrow("checkbox")            # options include disabled
+   r$id[w] <- "other"                            #    used by text, numeric, and select
+   r$label[w] <- "Other options"
+   r$options[w] <- "sameline; disabled"
+   r$inline[w] = TRUE
+   r$sameline[w] <- FALSE
+   r$width[w] <- "50%"
+imBlankforms[["spacer"]] <- r          # userType is "Spacer"
+
+### Hidden
+w=1
+   r <- imGetBlankFORMrow("text")
+   r$id[w] <- "label"
+   r$label[w] <- "Hidden name"
+   r$width[w] <- "75%"
+w=w+1
+   r[w,] <- imGetBlankFORMrow("text")
+   r$id[w] <- "value"
+   r$label[w] <- "Hidden value"
+   r$width[w] <- "75%"
+imBlankforms[["hidden"]] <- r          # userType is "Hidden"
+
+### Button
+w=1
+   r <- imColor
+w=w+1
+   r[w,] <- imGetBlankFORMrow("text")
+   r$id[w] <- "label"
+   r$label[w] <- "Button name"
+   r$placeholder[w] <- "Required..."
+   r$width[w] <- "15"
+w=w+1
+   r[w,] <- imGetBlankFORMrow("text")
+   r$id[w] <- "value"
+   r$label[w] <- "Button action"
+   r$placeholder[w] <- "Required..."
+   r$width[w] <- "15"
+w=w+1
+   r[w,] <- imGetBlankFORMrow("text")
+   r$id[w] <- "width"
+   r$label[w] <- "Width of button's &lt;div&gt;"
+   r$placeholder[w] <- "Blank means 100%..."
+   r$helptext[w] <- "Width in rems or 25%-33%-50%-66%-75%-100%"
+   r$width[w] <- "15"
+   r$sameline[w] <- FALSE
+w=w+1
+   r[w,] <- imGetBlankFORMrow("radio")
+   r$id[w] <- "placeholder"
+   r$label[w] <- "Position of button in the &lt;div&gt;"
+   r$options[w] <- "Left;Center;Right"
+   r$value[w] <- "Right"
+   r$inline[w] = TRUE
+w=w+1
+   r[w,] <- imGetBlankFORMrow("radio")
+   r$id[w] <- "btnsize"
+   r$label[w] <- "Button size"
+   r$options[w] <- "Extra-small;Small;Normal;Large"
+   r$value[w] <- "Normal"
+   r$inline[w] = TRUE
+w=w+1
+   r[w,] <- imGetBlankFORMrow("radio")
+   r$id[w] <- "btnshape"
+   r$label[w] <- "Button shape"
+   r$options[w] <- "Pill;Normal;Square"
+   r$value[w] <- "Normal"
+   r$inline[w] = TRUE
+   r$sameline[w] <- FALSE
+w=w+1
+   r[w,] <- imGetBlankFORMrow("text")
+   r$id[w] <- "options"
+   r$label[w] <- "Button class"
+   r$helptext[w] <- "Most useful for margins like mt-3 or my-1"
+   r$sameline[w] <- FALSE
+w=w+1
+   r[w,] <- imOther
+   r$sameline[w] <- FALSE
+imBlankforms[["button"]] <- r          # userType is "Button"
+
 ### TF_Chex input
 # This isn't working quite right and it was for Admins only. Leaving it out for now.
 # w=1
@@ -336,7 +462,7 @@ saveRDS(imBlankforms, file="app/Blankforms.RDS")
 ##################################################################################
 
 
-rm(r, w, imBlankforms, imGetBlankFORMrow, imName, imLabel, imHelptext, imWidth, imOther, imTable, imColumn)
+rm(r, w, imBlankforms, imGetBlankFORMrow, imName, imLabel, imHelptext, imWidth, imOther, imTable, imColumn, imColor)
 
 
 #imBlankforms <- readRDS(file="app/Blankforms.RDS")
